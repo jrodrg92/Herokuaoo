6b67582924c9472e42b77a743a98f9191b235d6a
Alastria <jrodrg92@gmail.com>
2020-05-25 19:24:15 +0200

    cambios node api

---
+++ src/server/api/blog.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import posts from '../../data/posts.json';
+import post from '../../data/post.json';
+
+const Router = express.Router();
+
+Router.get("/posts", (req, res, next) => {
+    res.json(posts);
+});
+
+Router.get("/post", (req, res, next) => {
+    res.json(post);
+});
+
+export default Router;
--- src/server/server.js
+++ src/server/server.js
@@ -4,8 +4,16 @@ import path from 'path';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import open from 'open';
+import exphbs from 'express-handlebars';
 
 import webpackConfig from '../../webpack.config.dev.js';
+
+//API
+import blogApi from './api/blog';
+
+import * as hbsHelper from '../lib/handlebars';
+
+import { isMobile } from '../lib/utils/device';
 
 //Server port
 const port = process.env.PORT || 3000;
@@ -19,6 +27,19 @@ const webPackCompiler = webpack(webpackConfig);
 //webpack middleware
 app.use(webpackDevMiddleware(webPackCompiler));
 app.use(webpackHotMiddleware(webPackCompiler));
+
+//API dispatch
+app.use('/api/blog', blogApi);
+
+//Handlerbar setup
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    helpers: hbsHelper
+}));
+
+//View engine setup
+app.set('views', path.join(__dirname, './views'));
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
