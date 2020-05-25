904a14a4e7e824c39f3669b945812f8e332d453b
Alastria <jrodrg92@gmail.com>
2020-05-25 20:43:36 +0200

    cambios api

--- src/server/index.js
+++ src/server/index.js
@@ -8,8 +8,6 @@ import exphbs from 'express-handlebars';
 
 import webpackconfig from '../../webpack.config.babel';
 
-<<<<<<< HEAD
-=======
 //API
 import blogApi from './api/blog';
 
@@ -18,7 +16,6 @@ import * as hbsHelper from '../lib/handlebars';
 import { isMobile } from '../lib/utils/device';
 
 //Server port
->>>>>>> origin/development
 const port = process.env.PORT || 3000;
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
@@ -27,32 +24,40 @@ const app= express();
 
 app.use(express.static(path.join(__dirname, '../public')))
 
+// Handlebars setup
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  helpers: hbsHelper
+}));
+  
+// View Engine Setup
+app.set('views', path.join(__dirname, 'views/'));
+app.set('view engine', '.hbs');
+
 const webpackCompiler = webpack(webpackconfig);
 if(isDevelopment){
     app.use(webpackDevmiddleware(webpackCompiler));
     app.use(webpackHotmiddleware(webpackCompiler));
 }
 
+app.use((req, res, next) => {
+    res.locals.isMobile = isMobile (req.headers['user-agent']);
+    return next();
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-<<<<<<< HEAD
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-=======
 //API dispatch
 app.use('/api/blog', blogApi);
 
-//Handlerbar setup
-app.engine('.hbs', exphbs({
-    extname: '.hbs',
-    helpers: hbsHelper
-}));
-
-//View engine setup
-app.set('views', path.join(__dirname, './views'));
+app.get('*', (req, res) => {
+  res.render('frontend/index', {
+      layout: false
+    });
+});
 
 app.listen(port, err => {
     if(!err){
         open('http://localhost:'+port);
     }
-})
+});
