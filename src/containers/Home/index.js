b9e83df5b34f7849b1ddf96ca7da99d652478e7b
Alastria <jrodrg92@gmail.com>
2020-05-27 01:15:28 +0200

    adding reducers

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,28 +1,30 @@
-// Dependencies
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Home extends Component {
-  static propTypes = {
-    isMobile: PropTypes.bool
-  };
 
-  render() {
-    const { isMobile } = this.props;
+    static propTypes = {
+        isMobile: PropTypes.bool
+    }
 
-    return (
-      <div className="Home">
-        <h1>Home Page</h1>
+    render() {
 
-        <p>
-          {isMobile ? 'Mobile device' : 'Desktop device'}
-        </p>
-      </div>
-    );
-  }
+        const { isMobile } = this.props;
+
+        return (
+          <div className="Home">
+            <p>Hola amigues</p>
+            <p>
+                {console.log(isMobile)}
+                {isMobile ? 'Mobile device' : 'Desktop device'}
+            </p>
+          </div>
+        );
+      }
 }
 
+// Metodo en ES6 conecta el estado y los metodos que acepta
 export default connect(state => ({
-  isMobile: state.device.isMobile
-}), null)(Home);
+    isMobile: state.device.isMobile
+  }), null)(Home);
--- src/reducers/deviceReducer.js
+++ src/reducers/deviceReducer.js
@@ -1,5 +1,5 @@
-export default function deviceReducer(state = {}){
+export default function deviceReducer(state = {}) {
     let isMobile = state.isMobile === 'false' ? false : true;
-
+  
     return Object.assign({}, state, { isMobile });
-}
+  }
