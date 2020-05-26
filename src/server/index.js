dfa1adf54e3d9b7a01a7ce8e18a4fe2087c46082
Alastria <jrodrg92@gmail.com>
2020-05-26 20:07:22 +0200

    cambios api

--- src/server/index.js
+++ src/server/index.js
@@ -4,20 +4,30 @@ import path from 'path';
 import webpackDevmiddleware from 'webpack-dev-middleware';
 import webpackHotmiddleware from 'webpack-hot-middleware';
 import open from 'open';
-import exphbs from 'express-handlebars';
 
+//webpack config
 import webpackconfig from '../../webpack.config.dev';
 
+//blogapi
+import blogApi from './api/blog';
+
+//enviroment
 const port = process.env.PORT || 3000;
 
+//express app
 const app= express();
 
 app.use(express.static(path.join(__dirname, '../public')))
 
+//webpack compiler
 const webpackCompiler = webpack(webpackconfig);
 app.use(webpackDevmiddleware(webpackCompiler));
 app.use(webpackHotmiddleware(webpackCompiler));
 
+//API dispatch
+app.use('/api/blog', blogApi);
+
+//Send traffic to react
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
